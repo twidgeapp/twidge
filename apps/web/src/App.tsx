96295d413137898beccb65dc131f64a50dc7960e
VarunPotti <potti.varun07@gmail.com>
2022-08-21 20:02:48 +0530

    feat: read autostart from db and enable/disable it based on the value

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -4,6 +4,7 @@ import { Route, Routes, useLocation } from 'react-router-dom';
 import ErrorFallback from './pages/error';
 import React, { useEffect } from 'react';
 import { invoke } from '@tauri-apps/api/tauri';
+import rspc from '@twidge/utils/query';
 
 const IndexPage = React.lazy(() => import('./pages/index'));
 const HomePage = React.lazy(() => import('./pages/home'));
@@ -11,10 +12,25 @@ const SpacePage = React.lazy(() => import('./pages/space'));
 
 function App() {
 	const location = useLocation();
+	const { data } = rspc.useQuery(['settings.get', { key: 'autostart' }]);
 
 	useEffect(() => {
-		invoke('plugin:autostart|enable').then(console.log).catch(console.error); // make sure to catch errors !
-	}, []);
+		if (data) {
+			const autostart = (data as any)['value'] === 'true';
+			if (autostart) {
+				invoke('plugin:autostart|enable')
+					.then(console.log)
+					.catch(console.error); // make sure to catch errors !
+			} else {
+				invoke('plugin:autostart|disable')
+					.then(console.log)
+					.catch(console.error); // make sure to catch errors !
+			}
+		}
+	}, [data]);
+
+	// useEffect(() => {
+	// }, []);
 
 	return (
 		<ErrorBoundary
