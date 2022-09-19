32c9d844200a205a1c0bfa7f581baf57c51c783c
Varun Potti <potti.varun07@gmail.com>
2022-09-19 23:02:03 +0530

    feat(wip): init infinite-viewer along with moveable elements

--- apps/web/src/pages/spaces/whiteboard.tsx
+++ apps/web/src/pages/spaces/whiteboard.tsx
@@ -1,12 +1,18 @@
 import { useSelector } from "@twidge/core/state";
 import { Spaces } from "@twidge/utils/bindings";
-import { useMemo } from "react";
+import InfiniteViewer from "react-infinite-viewer";
+import React, { useMemo } from "react";
 import { useParams } from "react-router";
 import SpaceContext from "../../components/spaces/ctx";
 import SpaceSidebar from "../../components/spaces/sidebar";
 import Layout from "../../layouts";
+import Moveable from "react-moveable";
+import { flushSync } from "react-dom";
+import Selecto from "selecto";
 
 const WhiteboardPage = () => {
+    const selectoRef = React.useRef(null);
+
     const params = useParams();
     const spaces = useSelector((state: any) => state.spaces.spaces);
 
@@ -22,6 +28,33 @@ const WhiteboardPage = () => {
         <SpaceContext.Provider value={{ spaceInfo: spaceInfo }}>
             <Layout>
                 <SpaceSidebar />
+
+                <InfiniteViewer
+                    useAutoZoom={true}
+                    useMouseDrag={true}
+                    usePinch={true}
+                    useWheelScroll={true}
+                    className="viewer"
+                >
+                    <div className="viewport selecto-area">
+                        <div className="target">asd</div>
+                        <Moveable
+                            target=".target"
+                            flushSync={flushSync}
+                            draggable={true}
+                            resizable={true}
+                            renderDirections={["nw", "ne", "se", "sw"]}
+                            snappable={true}
+                            origin={false}
+                            rotationPosition={"none"}
+                            rotateAroundControls={true}
+                            onRender={(e) => {
+                                e.target.style.cssText += e.cssText;
+                            }}
+                        />
+                    </div>
+                </InfiniteViewer>
+                <div className="empty elements"></div>
             </Layout>
         </SpaceContext.Provider>
     );
