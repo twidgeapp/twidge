49ef08d5f54a0074c802d9a72edf5fcc31fc7fa3
Varun Potti <potti.varun07@gmail.com>
2022-09-23 19:39:34 +0530

    chore: run cargo clippy

--- core/src/db/migrator.rs
+++ core/src/db/migrator.rs
@@ -1,23 +1,8 @@
-use std::sync::Arc;
-
 use crate::{
     errors::CoreError,
     prisma::{new_client_with_url, PrismaClient},
     utils::get_twidge_dir,
-    Shared,
 };
-use enumflags2::BitFlags;
-use include_dir::{include_dir, Dir};
-use log::info;
-use migration_core::migration_connector::ConnectorParams;
-use migration_core::{commands::apply_migrations, json_rpc::types::ApplyMigrationsInput};
-use quaint::prelude::*;
-use sql_migration_connector::SqlMigrationConnector;
-use tokio::fs::{create_dir, remove_dir_all};
-
-use super::settings;
-
-static MIGRATION_DIR: Dir = include_dir!("$CARGO_MANIFEST_DIR/../prisma/migrations");
 
 pub async fn new_client() -> Result<PrismaClient, CoreError> {
     let library_url = get_twidge_dir().join("library.db");
--- core/src/functions.rs
+++ core/src/functions.rs
@@ -1,5 +1,5 @@
 #[tauri::command]
-pub async fn show_bar(window: tauri::Window) -> Result<(), String> {
+pub async fn show_bar(_window: tauri::Window) -> Result<(), String> {
     #[cfg(target_os = "macos")]
     window.set_decorations(true);
 
