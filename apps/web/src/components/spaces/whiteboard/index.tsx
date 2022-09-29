8f133c6ca4c18a8d61130f5d464bc47a785f05af
Varun Potti <potti.varun07@gmail.com>
2022-09-29 10:09:42 +0530

    feat: support audio players

--- apps/web/src/components/spaces/whiteboard/index.tsx
+++ apps/web/src/components/spaces/whiteboard/index.tsx
@@ -4,9 +4,11 @@ import {convertFileSrc} from "@tauri-apps/api/tauri";
 
 export const checkIfImage = (file_type: string) => file_type === "png" || file_type === "jpeg" || file_type === "jpg" || file_type === "gif" || file_type === "svg"
 
-export const checkIfVideo = (file_type: string) => file_type === "mp4" || file_type === "webm" || file_type === "ogg";
+export const checkIfVideo = (file_type: string) => file_type === "mp4" || file_type === "webm" || file_type === "ogg" || file_type === "avi" || file_type === "mov" || file_type === "quicktime"
 
-export const checkIfMedia = (file_type: string) => checkIfImage(file_type) || checkIfVideo(file_type);
+export const checkIfAudio = (file_type: string) => file_type === "mp3" || file_type === "wav" || file_type === "ogg" || file_type === "flac" || file_type === "aac" || file_type === "mpeg" || file_type === "m4a"
+
+export const checkIfMedia = (file_type: string) => checkIfImage(file_type) || checkIfVideo(file_type)
 
 const Text = ({element}: { element: WhiteboardItem }) => {
     return (
@@ -20,9 +22,14 @@ const WhiteboardItemElement = ({element}: { element: WhiteboardItem }) => {
     if (element.item_type === 'text') {
         return <Text element={element}/>
     } else if (checkIfImage(element.item_type)) {
-        return <img className="rounded-md" src={convertFileSrc(element.value)}/>
+        return <img className="rounded-md h-full w-full" src={convertFileSrc(element.value)}/>
     } else if (checkIfVideo(element.item_type)) {
-        return <video className="rounded-md" src={convertFileSrc(element.value)} controls={true}/>
+        return <video className="rounded-md h-full w-full" src={convertFileSrc(element.value)} controls={true}/>
+    } else if (checkIfAudio(element.item_type)) {
+        return (
+            <audio className="rounded-md h-full w-full audio" src={convertFileSrc(element.value)}
+                   controls={true}/>
+        )
     }
 
     return <div>Unknown item type</div>
