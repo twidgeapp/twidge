091a8fb384e0cd3a098f5a38ce98e8f7c701919f
Varun Potti <potti.varun07@gmail.com>
2022-12-04 21:03:04 +0530

    feat: basic setup

---
+++ apps/desktop/src/App.tsx
@@ -0,0 +1,22 @@
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  )
+}
+
+export default App
